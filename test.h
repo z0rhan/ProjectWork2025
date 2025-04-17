b754a9479729e32a4ae9789f8e3e7dde1e07c5bb
z0rhan <uknowme1928@gmail.com>
2025-04-17 13:00:57 +0300

    Change the animation to match the business concept

---
+++ test.h
@@ -0,0 +1,547 @@
+// Initial smile face
+"e5f"
+"s6cs7cs9csac"
+"s6bs7bs9bsab"
+"s69sa9"
+"s78s88s98"
+
+// Press the button for next animation
+"x"
+
+// Wait for 3 seconds
+// "w0085"
+
+"e00a" // turn of for all
+
+// Text slide in
+
+// 'WELCOME'
+// 'W'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'L'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'C'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'O'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+
+// 'M'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+"p21"
+"p21"
+
+// Press button for next animation
+"x"
+
+// 'TERVETULOA'
+
+// 'T
+"e0fsbc"
+"w000a"
+"p21sbc"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'R'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb9"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'V'
+"e0fsbcsbbsba"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsbcsbbsba"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'T
+"e0fsbc"
+"w000a"
+"p21sbc"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'U'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'L'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21e0fsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'O'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+"p21"
+"p21"
+
+// 'A'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsba"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// Press button for next animation
+"x"
+
+// 'THANKS'
+
+// 'T
+"e0fsbc"
+"w000a"
+"p21sbc"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'H'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'A'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsba"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'N'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'K'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbbsb9"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'S'
+"e0fsbcsbasb9sb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// Press button for next animation
+"x"
+
+// 'KIITOS'
+
+// 'K'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbbsb9"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'I'
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'I'
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'T
+"e0fsbc"
+"w000a"
+"p21sbc"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'O'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'S'
+"e0fsbcsbasb9sb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// Press button for next animation
+"x"
+
+// 'SORRY'
+
+// 'S'
+"e0fsbcsbasb9sb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'O'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'R'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb9"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'R'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb9"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'Y'
+"e0fsbc"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsbasb9sb8"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsbc"
+"w000a"
+
+// Press button for next animation
+"x"
+
+// 'ANTEEKSI'
+
+// 'A'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsba"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'N'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbb"
+"w000a"
+"p21e0fsba"
+"w000a"
+"p21e0fsb9"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'T
+"e0fsbc"
+"w000a"
+"p21sbc"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21e0fsbc"
+"w000a"
+"p21"
+"p21"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'E'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'K'
+"e0fsb8sb9sbasbbsbc"
+"w000a"
+"p21e0fsbbsb9"
+"w000a"
+"p21e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// 'S'
+"e0fsbcsbasb9sb8"
+"w000a"
+"p21e0fsbcsbasb8"
+"w000a"
+"p21e0fsbcsbbsbasb8"
+"w000a"
+"p21"
+"p21"
+
+// 'I'
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21e0fsb8sb9sbasbbsbc"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"e0fsbcsb8"
+"w000a"
+"p21"
+"p21"
+
+// Press button for next animation
+"x"
+
+// Initial smile face
+"e5f"
+"s6cs7cs9csac"
+"s6bs7bs9bsab"
+"s69sa9"
+"s78s88s98"
+
+
+// Back to smiley face
+// "p21e0fsbcsbbsb9"
+// "w000a"
+// "p21e0fsbcsbbsb8"
+// "w000a"
+// "p21e0fsb8"
+// "w000a"
+// "p21e0fsbcsbbsb8"
+// "w000a"
+// "p21e0fsbcsbbsb9"
+// "w000a"
+// "p21"
+// 
+// // Blink for sometime
+// "w0085"
